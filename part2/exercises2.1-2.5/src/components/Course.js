d60dcc2a3ca06987607c16c6ecfad5a52aa5a3ee
Maxberman3 <mb3ax@virginia.edu>
2020-09-23 10:56:31 +0300

    Finished reading through 2b and taking notes

--- part2/exercises2.1-2.5/src/components/Course.js
+++ part2/exercises2.1-2.5/src/components/Course.js
@@ -3,16 +3,23 @@ import React from 'react'
 const Course=(props)=>{
   return (
     <div>
-    <h2>{props.course.name}</h2>
-    <ul>
-    {props.course.parts.map(part=><li key={part.id}>{part.name} {part.exercises}</li>)}
-    </ul>
-    <h5>The total number of exercises is: {props.course.parts.reduce((sum,part)=>{
-      // console.log(part)
-      // console.log(part.exercises)
-      // console.log(sum)
-      return sum+=part.exercises}
-      ,0)}</h5>
+    {props.courses.map((course)=>{
+      return(
+        <div>
+        <h2 key={course.id}>{course.name}</h2>
+        <ul>
+        {course.parts.map((part)=>{
+          // console.log(part.id)
+          return<li key={part.id}>{part.name}{part.exercises}</li>
+        })}
+        </ul>
+        <h5>The total number of exercises is {course.parts.reduce((sum,part)=>{
+          return sum+=part.exercises
+        },0)}</h5>
+        </div>
+      )
+    })}
+
     </div>
   )
 }
