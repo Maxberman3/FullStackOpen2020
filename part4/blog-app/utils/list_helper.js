94da9ee959dc7f176078f80b42fc5d3864c4df23
Maxberman3 <mb3ax@virginia.edu>
2020-10-05 16:52:02 +0300

    finished exercise 4.6

--- part4/blog-app/utils/list_helper.js
+++ part4/blog-app/utils/list_helper.js
@@ -1,3 +1,18 @@
+const _=require('lodash')
+
+const mostBlogs = (blogs) => {
+  const byAuthors=_.groupBy(blogs,'author')
+  const mostBlogsAuthored=_.reduce(byAuthors,(best,blogs,author)=>{
+    // console.log(author)
+    if(blogs.length>best.blogs){
+      best.author=author
+      best.blogs=blogs.length
+    }
+    return best
+  },{author:'',blogs:0})
+  return mostBlogsAuthored
+}
+
 const dummy = (blogs) => {
   return 1
 }
@@ -9,7 +24,19 @@ const totalLikes = blogs => {
   return blogs.reduce(reducer,0)
 }
 
+const favoriteBlog = blogs =>{
+  const reducer = (best,current) =>{
+    if(current.likes>best.likes){
+      best=current
+    }
+    return best
+  }
+  return blogs.reduce(reducer)
+}
+
 module.exports={
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
 }
