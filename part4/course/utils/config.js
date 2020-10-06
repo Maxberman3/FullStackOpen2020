bff67f486d35a1316de43eedbcbe7c5527ebe5c5
Maxberman3 <mb3ax@virginia.edu>
2020-10-06 17:57:22 +0300

    Read through the course material in part 4b

--- part4/course/utils/config.js
+++ part4/course/utils/config.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
+  // console.log('in test environment')
   MONGODB_URI = process.env.TEST_MONGODB_URI
+  // console.log(`read the environment var for db as ${MONGODB_URI}`)
 }
 
 module.exports = {
