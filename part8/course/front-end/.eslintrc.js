b62b343dcfa50fbe0496ff8f89c8fbae98f01e98
Maxberman3 <mb3ax@virginia.edu>
2020-10-29 17:15:54 +0200

    Reading through chapter 8d

--- part8/course/front-end/.eslintrc.js
+++ part8/course/front-end/.eslintrc.js
@@ -1,22 +1,20 @@
+//eslint-disable-next-line
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": 1,
+    "react/prop-types": 0
+  }
 };
--- part8/course/front-end/src/App.js
+++ part8/course/front-end/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import {useQuery, useState} from "@apollo/client";
+import {useQuery, useState, useApolloClient} from "@apollo/client";
 import Persons from "./components/Persons";
 import {ALL_PERSONS} from "./queries";
 import PersonForm from "./components/PersonForm";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
+  const client = useApolloClient();
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   const notify = message => {
     setErrorMessage(message);
@@ -15,9 +24,21 @@ const App = () => {
       setErrorMessage(null);
     }, 10000);
   };
-
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
   return (
     <div>
+      <button onClick={logout}>logout</button>
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
